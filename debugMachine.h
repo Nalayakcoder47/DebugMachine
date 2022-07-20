191079c5ea7a9185cd4e6110f2ba80734d823e2a
Ravijot singh <Ravijotsingh427@gmail.com>
2022-07-20 09:48:34 +0530

    Update debugMachine.h

--- debugMachine.h
+++ debugMachine.h
@@ -5,7 +5,7 @@
 inline class debugMachine {
 public:
 	void out (std::string toWrite, bool nl = true) {
-		if (write) {
+		if (toWrite) {
 			std::cout << toWrite;
 			if (nl) {
 				std::cout << std::endl;
@@ -13,7 +13,7 @@ public:
 		}
 	}
 	void warn (std::string toWrite, bool nl = true) {
-		if (write) {
+		if (toWrite) {
 			std::cout << "\033[31mWARNING: " << toWrite << "\033[0m";
 			if (nl) {
 				std::cout << std::endl;
@@ -21,7 +21,7 @@ public:
 		}
 	}
 	void info (std::string toWrite, bool nl = true) {
-		if (write) {
+		if (toWrite) {
 			std::cout << "\033[36mINFO: " << toWrite << "\033[0m";
 			if (nl) {
 				std::cout << std::endl;
@@ -29,7 +29,7 @@ public:
 		}
 	}
 	void debug (std::string toWrite, bool nl = true) {
-		if (write) {
+		if (toWrite) {
 			std::cout << "\033[35mDEBUG: " << toWrite << "\033[0m";
 			if (nl) {
 				std::cout << std::endl;
